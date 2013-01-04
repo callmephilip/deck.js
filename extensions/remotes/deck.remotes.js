dee2e4058b245950aaf68e2de568addad5ccfed0
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-04 11:14:21 -0800

    add remote control for presentations

---
+++ extensions/remotes/deck.remotes.js
@@ -0,0 +1,18 @@
+/*!
+Deck JS - deck.remotes
+Copyright (c) 2013 Philip Nuzhnyi
+*/
+
+(function($, deck, undefined) {
+    var $d = $(document),
+    remotesLibPath = 'https://raw.github.com/Remotes/Remotes/master/dist/remotes.ne.min.js';
+    
+    head.js(remotesLibPath, function(){ 
+        new Remotes("preview")
+            .on("swipe-left", function(e){ $[deck]('next'); })
+            .on("swipe-right", function(e){ $[deck]('prev'); })
+            .on("tap", function(e){ $[deck]('next'); });
+    });
+
+})(jQuery, 'deck');
+
