cc5caea099984ed378a048e8372726e05bf6abf5
callmephilip <philip.nuzhnyy@gmail.com>
2013-01-04 11:29:41 -0800

    remove head.js loader and use modernizr's resource loader instead

--- extensions/remotes/deck.remotes.js
+++ extensions/remotes/deck.remotes.js
@@ -7,12 +7,17 @@ Copyright (c) 2013 Philip Nuzhnyi
     var $d = $(document),
     remotesLibPath = 'https://raw.github.com/Remotes/Remotes/master/dist/remotes.ne.min.js';
     
-    head.js(remotesLibPath, function(){ 
-        new Remotes("preview")
-            .on("swipe-left", function(e){ $[deck]('next'); })
-            .on("swipe-right", function(e){ $[deck]('prev'); })
-            .on("tap", function(e){ $[deck]('next'); });
-    });
+    Modernizr.load([
+        {
+            load: remotesLibPath,
+            complete: function () {
+                new Remotes("preview")
+                    .on("swipe-left", function(e){ $[deck]('next'); })
+                    .on("swipe-right", function(e){ $[deck]('prev'); })
+                    .on("tap", function(e){ $[deck]('next'); });
+            }
+        }
+    ]);
 
 })(jQuery, 'deck');
 
